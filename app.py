f6c2264dab379d48700056bb490a7bdba4c707df
Gabe Mitnick <gabe.mitnick@gmail.com>
2018-07-18 16:15:23 -0700

    switch to python3 and revert to only single-letter answers

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import constants
 
 QUESTIONS = constants.QUESTIONS
-# sum of weights of questions answered "no"
+# sum of weights of questions answered 'no'
 points = 0
 # sum of weights of all questions
 max = 0
 
-print("Welcome to rice purity 2.0. Answer y for yes and n for no.")
-for question, weight in QUESTIONS.iteritems():
-	response = ""
+print('Welcome to rice purity 2.0. Answer y for yes and n for no.')
+for question, weight in QUESTIONS.items():
+	response = ''
 	# ask until a valid response is given
-	while response[0].lower() in ["y", "n"]:
-		response = raw_input(question + "\n\t")
-	# if "n", purity increases
-	if response[0].lower() == "n":
+	print(question)
+	# rip do-while loops :(
+	response = input()
+	while not response.lower() in ['y', 'n']:
+		print('Please respond either "y" or "n"')
+		response = input()
+	# if 'n', purity increases
+	if response.lower() == 'n':
 		points += weight
 	# maximum increases no matter what
 	max += weight
 
-score = points * 100 / max
-print("\n\n")
-print("Your final score is " + str(score))
+score = int(points * 100 / max)
+print('\n\n')
+print('Your final score is ' + str(score))
